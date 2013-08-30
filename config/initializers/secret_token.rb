ff4936bbcad6b4490dce03a13fec4807ad6320cc
Antonio <abiallo@gmail.com>
2013-08-30 16:43:17 +0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloant2::Application.config.secret_token = '6515706bc9119bacfde99ad9b27f246ca6e8187175a4fa415d41bde0480354d1bc7c30ecb351f5d382a1afdab37c982a4196798c94f235b3451e790dfaf87741'
